322da5b9b498d745b5eca013f5926b9e1863a160
Henrique <you@example.com>
2023-12-22 15:40:32 -0300

    feat: add set publication date method

--- contents/models.py
+++ contents/models.py
@@ -6,3 +6,7 @@ class Content(models.Model):
     body = models.TextField(verbose_name='Conteúdo do texto')
     author = models.CharField(max_length=100, verbose_name='Autor')
     publication_date = models.DateTimeField(default=timezone.now, verbose_name='Data de Publicação')
+
+    def set_publication_date(self):
+        self.publication_date = timezone.now()
+        super().save()
