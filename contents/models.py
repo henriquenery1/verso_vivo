6c9c6620c769f477e6bbf4c3cb0da252da21c346
Henrique <henriquenery07@gmail.com>
2023-12-27 11:08:52 -0300

    feat: change format data

--- contents/models.py
+++ contents/models.py
@@ -5,3 +5,6 @@ class Content(models.Model):
     title = models.CharField(max_length=200, verbose_name='Título')
     body = models.TextField(verbose_name='Conteúdo')
     publication_date = models.DateTimeField(default=timezone.now, verbose_name='Data de Publicação')
+
+    def formatted_date(self):
+        return self.publication_date.strftime('%d/%m/%Y')
